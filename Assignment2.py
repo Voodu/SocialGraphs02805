875e544ed365b1749e09bb42d0f36c9faddffb23
Piotr Ładoński <piolado@gmail.com>
2020-11-03 01:16:07 +0100

    Remove wikisyntax in tf list

--- Assignment2.py
+++ Assignment2.py
@@ -351,7 +351,7 @@ def get_tf_list(universe):
     tf = {}
     for filename in glob.glob(f'{universe}/*.txt'):
         with io.open(filename, 'r', encoding='utf8') as f:
-            tokens = tokenize_text(f.read())
+            tokens = tokenize_text(f.read(), True)
             for word, count in FreqDist(tokens).items():
                 tf[word] = tf.setdefault(word, 0) + count
     pickle.dump(tf, io.open(f'tf_{universe}.p', 'wb'))
